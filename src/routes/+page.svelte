ff0e7276cb2ad7e91e38a89ddf8e8599a83354b8
Dan Grebb <git@dgrebb.com>
2024-03-02 02:27:02 -0500

    wip(app): working websocket via `setInterval` on the way to realtime

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,110 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	// import { onMount } from 'svelte';
+	// $lib auto-resolves to ./src/lib in Svelte.
+	// import { state, connect } from '$lib/state';
+	import { Button } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	import { Card } from 'flowbite-svelte';
+
+	let webSocketEstablished = false;
+	let ws: WebSocket | null = null;
+	let log: string[] = [];
+
+	const logEvent = (str: string) => {
+		log = [...log, str];
+	};
+
+	const establishWebSocket = () => {
+		if (webSocketEstablished) return;
+		const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+		ws = new WebSocket(`${protocol}//${window.location.host}/websocket`);
+		ws.addEventListener('open', (event) => {
+			webSocketEstablished = true;
+			console.log('[websocket] connection open', event);
+			logEvent('[websocket] connection open');
+		});
+		ws.addEventListener('close', (event) => {
+			console.log('[websocket] connection closed', event);
+			logEvent('[websocket] connection closed');
+		});
+		ws.addEventListener('message', (event) => {
+			console.log('[websocket] message received', event);
+			logEvent(`[websocket] message received: ${event.data}`);
+		});
+	};
+
+	const requestData = async () => {
+		const res = await fetch('/api/test');
+		const data = await res.json();
+		console.log('Data from GET endpoint', data);
+		// logEvent(`[GET] data received: ${JSON.stringify(data)}`);
+		return data;
+	};
+
+	const closeSocket = () => {
+		ws?.close();
+	};
+
+	interface Item {
+		title: string;
+		content: {
+			small: {
+				value: string;
+				color: string;
+			};
+			large: {
+				value: string;
+				color: string;
+			};
+		};
+	}
+
+	// export let data;
+	let items: Item[] = [];
+
+	onMount(async () => {
+		setInterval(async () => {
+			const { data } = await requestData();
+			console.log('🚀 ~ onMount ~ data:', data);
+			items = data.items;
+		}, 3333);
+
+		console.log('🚀 ~ items:', items);
+	});
+</script>
+
+<main>
+	<Button on:click={closeSocket}>Stop Socket</Button>
+	<h1>SvelteKit with WebSocket Integration</h1>
+
+	<button disabled={webSocketEstablished} on:click={() => establishWebSocket()}>
+		Establish WebSocket connection
+	</button>
+
+	<button on:click={() => requestData()}> Request Data from GET endpoint </button>
+
+	<ul>
+		{#each log as event}
+			<li>{event}</li>
+		{/each}
+	</ul>
+
+	<div class="p-8">
+		{#each items as { title, content: { small, large } }}
+			<Card>
+				<h2>{title}</h2>
+				<h1 class="text-7xl">{large.value}</h1>
+				<p>{small.value}</p>
+			</Card>
+		{/each}
+	</div>
+</main>
+
+<style>
+	h1 {
+		color: red;
+	}
+	main {
+		font-family: sans-serif;
+	}
+</style>
---
+++ src/routes/api/test/+server.ts
@@ -0,0 +1,51 @@
+import { json } from '@sveltejs/kit';
+import { SECRET_NIGHTSCOUT_TOKEN } from '$env/static/private';
+import type { RequestHandler } from './$types';
+
+export const GET = (async ({ url, locals }) => {
+  if (locals.wss) {
+    locals.wss.clients.forEach(client => {
+      if (client.readyState === 1) {
+        client.send(`Hello from the GET handler at ${new Date().toLocaleString()}`);
+      }
+    });
+  }
+
+	// call server for data
+	const requestOptions = {
+		method: 'GET',
+		redirect: 'follow'
+	};
+
+	const [test] = await Promise.all([
+		fetch(
+			`https://glu.7ub3s.net/api/v1/entries.json?count=1&token=${SECRET_NIGHTSCOUT_TOKEN}`,
+			requestOptions
+		)
+			.then((response) => response.json())
+			.catch((error) => console.error(error))
+	]);
+
+	return json({
+    success: true,
+    data: {
+      items: [
+        {
+          title: 'glu',
+          content: {
+            small: {
+              value: 'mg/dl',
+              color: '#CC4522'
+            },
+            large: {
+              value: test[0]['sgv'],
+              color: '#A5371B'
+            }
+          }
+        }
+      ]
+    }
+	});
+
+	return json({ success: true, message: 'Hello world from GET handler', url });
+}) satisfies RequestHandler;
