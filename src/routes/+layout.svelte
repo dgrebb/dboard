8fdbd730dc5ec05ae445e8f7112216f5d988688a
Dan Grebb <git@dgrebb.com>
2024-03-02 02:25:13 -0500

    feat(app): add tailwind config, layout, and refresh button
    adds basic configuration for tailwind, darkmode via flowbite, some basic layout styles, and a
    function to refresh components by invalidating server state

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { invalidateAll } from '$app/navigation';
+	import '../app.pcss';
+	import '../app.pcss';
+	import { Button, DarkMode } from 'flowbite-svelte';
+	import { RefreshOutline } from 'flowbite-svelte-icons';
+
+	function refresh() {
+		invalidateAll();
+	}
+</script>
+
+<main>
+	<header class="flex flex-row justify-between p-3">
+		<DarkMode></DarkMode>
+		<Button size="sm" color="dark" on:click={() => refresh()}><RefreshOutline /></Button>
+	</header>
+	<slot />
+</main>
