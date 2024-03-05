05b4fe19ffa3cb2afabf4718abadf7ffa5e0780b
Dan Grebb <git@dgrebb.com>
2024-03-04 22:53:11 -0500

    style(app): round temp instead of ceil

--- src/lib/widgets/CurrentWeather.svelte
+++ src/lib/widgets/CurrentWeather.svelte
@@ -8,6 +8,6 @@
   <Barometer size="50" class="mb-3 self-end text-gray-500 dark:text-gray-400" />
   <h2>Lansdale</h2>
   <h1 class="mt-auto justify-end text-9xl text-blue-500">
-    {Math.ceil(weather.current)}<span class="text-gray-50">&deg;</span>
+    {Math.round(weather.current)}<span class="text-gray-50">&deg;</span>
   </h1>
 </Card>
