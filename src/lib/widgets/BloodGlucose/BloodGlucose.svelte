8c366d145b54e19520cb2b9b5ea0a7d991485ae1
Dan Grebb <git@dgrebb.com>
2024-03-04 22:53:44 -0500

    feat(app): add echarts and BloodGlucose graph component

---
+++ src/lib/widgets/BloodGlucose/BloodGlucose.svelte
@@ -0,0 +1,48 @@
+<script>
+  import * as echarts from 'echarts';
+  import { onMount } from 'svelte';
+
+  // Draw the chart
+
+  onMount(() => {
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom, 'dark');
+    var option;
+
+    option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line',
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  });
+</script>
+
+<div class="graph-container">
+  <div id="main" class="bg-graph" style="width: 100%; height: 100%;"></div>
+  <p>chart</p>
+</div>
+
+<style>
+  .graph-container {
+    height: 7rem;
+    width: 100%;
+    z-index: 1;
+    position: relative;
+  }
+  .bg-graph {
+    width: 100%;
+    min-height: 100%;
+  }
+</style>
