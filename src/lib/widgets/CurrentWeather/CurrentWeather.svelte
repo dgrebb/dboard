8bc117d5e2bbd9d64946df93ab06b5846f4c14a6
Dan Grebb <git@dgrebb.com>
2024-03-20 21:43:23 -0400

    style(app): use bg chart as card background

--- src/lib/widgets/CurrentWeather/CurrentWeather.svelte
+++ src/lib/widgets/CurrentWeather/CurrentWeather.svelte
@@ -13,10 +13,6 @@
 {#if currentTemp}
   <div transition:fade class="card-container relative">
     <Card size="xl">
-      <Barometer
-        size="50"
-        class="mb-3 self-end text-gray-500 dark:text-gray-400"
-      />
       <h2>Lansdale</h2>
       <h1
         class="mt-auto justify-end text-9xl"
