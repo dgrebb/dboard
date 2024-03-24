8b178e9c79f3c32fc2bf9a4132c74afcb8182877
Dan Grebb <git@dgrebb.com>
2024-03-24 19:17:07 -0400

    style(weather): further adjust colors for light/dark contrast

--- src/lib/widgets/CurrentWeather/CurrentWeather.svelte
+++ src/lib/widgets/CurrentWeather/CurrentWeather.svelte
@@ -43,10 +43,10 @@
       <h2>Lansdale</h2>
       <WeatherIcon {weatherCode} {isDay} />
       <h1
-        class="dboard__card--value-lg dark:brightness-175 mt-auto justify-end text-9xl text-[var(--text-color)] brightness-125"
+        class="dboard__card--value-lg mt-auto justify-end text-9xl text-[var(--text-color)] brightness-75 dark:saturate-200"
       >
         {Math.round(current)}<span
-          class="dboard__card__value-symbol text-[var(--text-color)] brightness-125 dark:brightness-150"
+          class="dboard__card__value-symbol text-[var(--text-color)] brightness-150 dark:brightness-150"
           >&deg;</span
         >
       </h1>
