50ad11dc123bcb4b30d21a6159265b13b4f41003
Dan Grebb <git@dgrebb.com>
2024-03-28 17:52:45 -0400

    feat(app): add time-of day background colors and transitions

--- src/lib/GLOBALS.js
+++ src/lib/GLOBALS.js
@@ -1,4 +1,6 @@
 export const DEFAULT_REFRESH_INTERVAL = 60000,
   WEATHER_API = 'https://api.open-meteo.com/v1',
   WEATHER_LAT_LONG = 'latitude=40.2415&longitude=-75.2838',
+  LATITUDE = '40.2415',
+  LONGITUDE = '-75.2838',
   NIGHTSCOUT_API = 'https://glu.7ub3s.net/api/v1';
