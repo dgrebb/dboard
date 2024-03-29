204f78077e0902acc12c67ace2262f23900f1927
Dan Grebb <git@dgrebb.com>
2024-03-28 22:14:26 -0400

    feat(app): add weather background (for night)

---
+++ src/lib/components/Board/BackgroundFrame.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { ComponentType } from 'svelte';
+  import './Backgrounds/styles.css';
+
+  export let component: ComponentType;
+</script>
+
+<div
+  class="background-component height-full width-full absolute inset-0 flex overflow-hidden"
+>
+  <svelte:component this={component} />
+</div>
