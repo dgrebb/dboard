2a505e1c5cf2a1a373aeae55e2ed6c242b413fc9
Dan Grebb <git@dgrebb.com>
2024-03-30 02:41:38 -0400

    test(time): add time slider component and store

--- src/routes/(layouts)/Main.svelte
+++ src/routes/(layouts)/Main.svelte
@@ -3,13 +3,30 @@
   import '$lib/app.pcss';
   import BackgroundFrame from '$lib/components/Board/BackgroundFrame.svelte';
   import ClearOrCloudy from '$lib/components/Board/Backgrounds/ClearOrCloudy.svelte';
+  import Slider from '$lib/components/TimeMachine/Slider.svelte';
   import { Button, DarkMode } from 'flowbite-svelte';
   import { RefreshOutline } from 'flowbite-svelte-icons';
+
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const minutesToday = hours * 60 + minutes;
+
+  import { time } from '$lib/store';
+  import { onDestroy } from 'svelte';
+
+  $time = minutesToday;
+
+  let timeValue: number;
+  const unsubscribe = time.subscribe((value) => {
+    timeValue = value;
+  });
 
   function refresh() {
     invalidateAll();
     localStorage.removeItem('color-theme');
   }
+  onDestroy(unsubscribe);
 </script>
 
 <BackgroundFrame component={ClearOrCloudy} />
@@ -18,6 +35,9 @@
   <header class="flex flex-row justify-between p-3 opacity-50">
     <slot name="additional-controls" />
     <DarkMode class="mix-blend-difference" />
+    <p class="timemachine__time">
+      {Math.trunc(timeValue / 60)}:{timeValue % 60}
+    </p>
     <Button
       size="sm"
       color="light"
@@ -27,6 +47,13 @@
     >
   </header>
   <slot />
+  <Slider />
 </main>
 
 <slot name="footer" />
+
+<style>
+  .timemachine__time {
+    color: white;
+  }
+</style>
