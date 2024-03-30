1eb464e897025b5e4515dd161708173c02c4ea63
Dan Grebb <git@dgrebb.com>
2024-03-30 02:41:38 -0400

    test(time): add time slider component and store

---
+++ src/lib/components/TimeMachine/Slider.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Range } from 'flowbite-svelte';
+  import { time } from '$lib/store';
+  import { onDestroy } from 'svelte';
+
+  let minutesPerDay = 24 * 60;
+  let timeOfDay = 12 * 60;
+</script>
+
+<div class="time-machine">
+  <Range min="0" max={minutesPerDay} size="md" bind:value={$time} />
+  <p>Time: {timeOfDay}</p>
+</div>
+
+<style>
+  .time-machine {
+    z-index: 1;
+    position: relative;
+  }
+</style>
