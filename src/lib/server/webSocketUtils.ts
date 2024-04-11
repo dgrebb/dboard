58749945bfcc11f7ca8b61212c05e1029b640348
Dan Grebb <git@dgrebb.com>
2024-04-10 20:33:59 -0400

    feat(music): add music api

--- src/lib/server/webSocketUtils.ts
+++ src/lib/server/webSocketUtils.ts
@@ -10,7 +10,7 @@ export const GlobalThisWSS = Symbol.for('sveltekit.wss');
 export interface ExtendedWebSocket extends WebSocketBase {
   socketId: string;
   // userId: string;
-};
+}
 
 // You can define server-wide functions or class instances here
 // export interface ExtendedServer extends Server<ExtendedWebSocket> {};
@@ -21,29 +21,35 @@ export type ExtendedGlobal = typeof globalThis & {
   [GlobalThisWSS]: ExtendedWebSocketServer;
 };
 
-export const onHttpServerUpgrade = (req: IncomingMessage, sock: Duplex, head: Buffer) => {
+export const onHttpServerUpgrade = (
+  req: IncomingMessage,
+  sock: Duplex,
+  head: Buffer
+) => {
   const pathname = req.url ? parse(req.url).pathname : null;
   if (pathname !== '/websocket') return;
 
   const wss = (globalThis as ExtendedGlobal)[GlobalThisWSS];
 
   wss.handleUpgrade(req, sock, head, (ws) => {
-    console.log('[handleUpgrade] creating new connecttion');
+    // console.log('[handleUpgrade] creating new connecttion');
     wss.emit('connection', ws, req);
   });
 };
 
 export const createWSSGlobalInstance = () => {
-  const wss = new WebSocketServer({ noServer: true }) as ExtendedWebSocketServer;
+  const wss = new WebSocketServer({
+    noServer: true,
+  }) as ExtendedWebSocketServer;
 
   (globalThis as ExtendedGlobal)[GlobalThisWSS] = wss;
 
   wss.on('connection', (ws) => {
     ws.socketId = nanoid();
-    console.log(`[wss:global] client connected (${ws.socketId})`);
+    // console.log(`[wss:global] client connected (${ws.socketId})`);
 
     ws.on('close', () => {
-      console.log(`[wss:global] client disconnected (${ws.socketId})`);
+      // console.log(`[wss:global] client disconnected (${ws.socketId})`);
     });
   });
 
--- src/routes/api/music/+server.ts
+++ src/routes/api/music/+server.ts
@@ -1,18 +1,19 @@
-// import { json } from '@sveltejs/kit';
-// import { musicInfo } from '$lib/music';
+import { json } from '@sveltejs/kit';
 
-// import type { RequestHandler } from './$types';
+import type { RequestHandler } from './$types';
 
-// export const GET = (async ({ url, locals }) => {
-//   if (locals.wss) {
-//     locals.wss.clients.forEach((client) => {
-//       if (client.readyState === 1) {
-//         client.send(
-//           `Hello from the GET handler at ${new Date().toLocaleString()}`
-//         );
-//       }
-//     });
-//   }
+export const GET = (async ({ fetch, locals }) => {
+  const music = await fetch('/music.json');
+  const response = JSON.parse(await music.text());
+  if (locals.wss) {
+    locals.wss.clients.forEach((client) => {
+      if (client.readyState === 1) {
+        client.send(
+          `Hello from the GET handler at ${new Date().toLocaleString()}`
+        );
+      }
+    });
+  }
 
-//   return json({ success: true, ...musicInfo });
-// }) satisfies RequestHandler;
+  return json({ success: true, ...response });
+}) satisfies RequestHandler;
