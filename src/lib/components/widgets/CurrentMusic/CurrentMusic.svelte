22c915aedfc24570166e5cab20d4826bdb291def
Dan Grebb <git@dgrebb.com>
2024-04-10 20:35:14 -0400

    refactor(music): rework album artwork transitions and CurrentMusic styles

--- src/lib/components/widgets/CurrentMusic/CurrentMusic.svelte
+++ src/lib/components/widgets/CurrentMusic/CurrentMusic.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+  import AudioWave from '$lib/components/Animations/AudioWave.svelte';
+  import type { MusicData } from '$lib/types';
   import { onMount, onDestroy } from 'svelte';
   import { fade } from 'svelte/transition';
 
-  let file: string;
-  let PUBLIC_MUSIC_TITLE: string,
-    PUBLIC_MUSIC_ARTIST: string,
-    PUBLIC_MUSIC_ALBUM: string;
-  $: file = '';
+  let file: string, title: string, album: string, artist: string;
+  $: file = '/album_art.png';
+  $: newFile = '';
   $: modal = false;
+  $: title = '';
+  $: album = '';
+  $: artist = '';
+  $: transitionImages = false;
 
   const refreshInterval = 5000; // Interval in milliseconds to check for updates
   let refreshTimer: NodeJS.Timeout; // Timer to periodically check for updates
@@ -31,8 +35,23 @@
       if (response.ok) {
         const currentContent = await response.text();
         if (currentContent !== previousContent) {
-          file = `/album_art.png?_=${timestamp}`; // Update the file path if the file content has changed
           previousContent = currentContent; // Update the previous file content
+          const music = await fetch('/api/music');
+          ({ album, title, artist } = await music.json());
+          transitionImages = true;
+
+          const reset = function () {
+            setTimeout(() => {
+              file = newFile;
+              newFile = '';
+              transitionImages = false;
+            }, 1000);
+          };
+
+          setTimeout(() => {
+            reset();
+            newFile = `/album_art.png?_=${timestamp}`;
+          }, 1000);
         }
       } else {
         file = ''; // Clear the file path if the file does not exist
@@ -41,8 +60,6 @@
     } catch (error) {
       console.error('Error polling file:', error);
     } finally {
-      ({ PUBLIC_MUSIC_TITLE, PUBLIC_MUSIC_ARTIST, PUBLIC_MUSIC_ALBUM } =
-        await import('$env/static/public'));
       refreshTimer = setTimeout(pollFile, refreshInterval); // Schedule the next poll
     }
   }
@@ -53,22 +70,43 @@
 </script>
 
 {#if !modal}
-  <div
-    class="current-music"
-    transition:fade
-    on:click={toggleModal}
-    on:keydown={toggleModal}
-    role="switch"
-    tabindex="0"
-    aria-checked="false"
-  >
+  <div class="current-music" transition:fade>
     <div class="current-music__info flex flex-col">
-      <h1>{PUBLIC_MUSIC_TITLE}</h1>
-      <h2>{PUBLIC_MUSIC_ARTIST}</h2>
-      <h3>{PUBLIC_MUSIC_ALBUM}</h3>
+      <h1>{title}</h1>
+      <h2>{artist}</h2>
+      <h3>{album}</h3>
     </div>
-    {#key file}
-      <img src={file} alt="{PUBLIC_MUSIC_ALBUM} Artwork" />
+    {#key newFile}
+      {#if newFile.length < 1}
+        <!-- svelte-ignore a11y-no-noninteractive-element-to-interactive-role -->
+        <img
+          src={file}
+          alt="hi"
+          on:click={toggleModal}
+          on:keydown={toggleModal}
+          role="switch"
+          tabindex="0"
+          aria-checked="false"
+          class="transition-opacity {transitionImages
+            ? 'opacity-0'
+            : 'opacity-100'}"
+        />
+      {/if}
+      {#if newFile.length > 0}
+        <!-- svelte-ignore a11y-no-noninteractive-element-to-interactive-role -->
+        <img
+          src={newFile}
+          alt="hi"
+          on:click={toggleModal}
+          on:keydown={toggleModal}
+          role="switch"
+          tabindex="0"
+          aria-checked="false"
+          class="transition-opacity {transitionImages
+            ? 'opacity-100'
+            : 'opacity-0'}"
+        />
+      {/if}
     {/key}
   </div>
 {/if}
@@ -81,11 +119,12 @@
     on:click={toggleModal}
     on:keydown={toggleModal}
   >
-    <div class="current-music__modal__info flex flex-col">
-      <h2>{PUBLIC_MUSIC_ARTIST}</h2>
-      <h1>{PUBLIC_MUSIC_TITLE}</h1>
-      <h3>{PUBLIC_MUSIC_ALBUM}</h3>
-    </div>
     <img src={file} alt="" />
+    <div class="current-music__modal__info flex flex-col">
+      <h1>{title}</h1>
+      <h2>{artist}</h2>
+      <h3>{album}</h3>
+    </div>
+    <AudioWave />
   </div>
 {/if}
