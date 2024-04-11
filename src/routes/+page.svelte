fe377286c7d9ff2af8d5f7c9ef3e4bd491d932c0
Dan Grebb <git@dgrebb.com>
2024-04-10 20:35:14 -0400

    refactor(music): rework album artwork transitions and CurrentMusic styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,21 +83,21 @@
           document.documentElement.classList.toggle('light', true);
           document.documentElement.classList.toggle('dark', false);
           localStorage.setItem('color-theme', 'light');
-          console.log(isDay, 'day');
+          // console.log(isDay, 'day');
           break;
 
         case 0:
           document.documentElement.classList.toggle('light', false);
           document.documentElement.classList.toggle('dark', true);
           localStorage.setItem('color-theme', 'dark');
-          console.log(isDay, 'night');
+          // console.log(isDay, 'night');
           break;
 
         default:
           document.documentElement.classList.toggle('light', false);
           document.documentElement.classList.toggle('dark', true);
           localStorage.setItem('color-theme', 'dark');
-          console.log(isDay, 'night');
+          // console.log(isDay, 'night');
           break;
       }
     }
