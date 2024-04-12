a5751b99b61f82dd0af2ee887211c5fe6809df6d
Dan Grebb <git@dgrebb.com>
2024-04-11 22:03:57 -0400

    feat(app): add production build configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
+  precompress: false,
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      precompress: false,
+    }),
     alias: {
       $assets: 'static',
     },
