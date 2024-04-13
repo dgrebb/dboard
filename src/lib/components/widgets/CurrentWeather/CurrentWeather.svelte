78e4d10dbf221912866e85355e55a4a83359b56b
Dan Grebb <git@dgrebb.com>
2024-04-13 19:25:58 -0400

    feat(weather): add animated weather icons

--- src/lib/components/widgets/CurrentWeather/CurrentWeather.svelte
+++ src/lib/components/widgets/CurrentWeather/CurrentWeather.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte';
   import fahrenheitToColorShade from './tempColor';
   import { fade } from 'svelte/transition';
   import { Card } from 'flowbite-svelte';
@@ -10,6 +11,9 @@
   $: ({
     temperature_2m: current,
     weather_code: weatherCode,
+    wind_speed_10m: windSpeed,
+    wind_direction_10m: windDirection,
+    wind_gusts_10m: windGust,
     is_day: isDay,
   } = $weather);
   $: highlightColor = fahrenheitToColorShade(current);
@@ -26,7 +30,7 @@
   tabindex="-1"
   transition:fade
   role="button"
-  class="dboard__grid__item relative border-4 transition-colors {touched
+  class="dboard__grid__item current-weather relative border-4 transition-colors {touched
     ? `border-blue-800`
     : `border-transparent`}"
 >
@@ -37,7 +41,19 @@
     >
       <h2 class="text-[var(--mainColor)] brightness-75 dark:saturate-200">
         Lansdale
+        <Icon
+          icon="ei:arrow-up"
+          class="current-weather__wind-direction inline-block mix-blend-darken"
+          width={33}
+          style={`transform: rotate(${(windDirection + 180) % 360}deg); filter: brightness(33%)`}
+          color={highlightColor}
+        />
       </h2>
+      <div class="current-weather__wind">
+        <p class="text-sm">
+          {windSpeed} ↝ {windGust}mph
+        </p>
+      </div>
       <WeatherIcon {weatherCode} {isDay} />
       <h1
         class="dboard__card--value-lg mt-auto justify-end text-9xl text-[var(--mainColor)] brightness-75 dark:saturate-200"
