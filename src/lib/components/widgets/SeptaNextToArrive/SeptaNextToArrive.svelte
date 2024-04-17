4e31f6e8013ffc92290b20da2e0a9a8fc3e24f19
Dan Grebb <git@dgrebb.com>
2024-04-17 00:13:48 -0400

    feat(septa): add septa api and SeptaNextToArrive component

---
+++ src/lib/components/widgets/SeptaNextToArrive/SeptaNextToArrive.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { SeptaDataNextToArrive } from '$lib/types';
+  export let schedule: SeptaDataNextToArrive[];
+  console.log('🚀 ~ schedule:', schedule);
+</script>
+
+{#if schedule}
+  {#each schedule as train}
+    <ul class="septa">
+      <li class="septa__train">
+        <dl>
+          <dt>Train</dt>
+          <dd>{train.orig_train}</dd>
+        </dl>
+        <dl>
+          <dt>Departing</dt>
+          <dd>{train.orig_departure_time}</dd>
+        </dl>
+        <dl>
+          <dt>Arriving</dt>
+          <dd>{train.orig_arrival_time}</dd>
+        </dl>
+        <dl>
+          <dt>Delay</dt>
+          <dd>{train.orig_delay}</dd>
+        </dl>
+      </li>
+    </ul>
+  {/each}
+{/if}
+
+<style>
+  /* your styles go here */
+</style>
--- src/routes/api/septa/+server.ts
+++ src/routes/api/septa/+server.ts
@@ -1,1 +1,28 @@
-// TODO: Septa API
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import type { FetchOptions, SeptaDataNextToArrive } from '$lib/types';
+
+export const GET = (async ({ url }) => {
+  const headers = new Headers();
+  headers.append('accept', 'application/json');
+
+  const requestOptions: FetchOptions = {
+    method: 'GET',
+    headers,
+    redirect: 'follow',
+  };
+
+  const schedule: SeptaDataNextToArrive = await fetch(
+    'https://www3.septa.org/api/NextToArrive/index.php?req1=Pennbrook&req2=Suburban%20Station&req3=5',
+    requestOptions
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+
+  return json({
+    success: true,
+    schedule,
+  });
+
+  return json({ success: true, message: 'Hello world from GET handler', url });
+}) satisfies RequestHandler;
