24202a8580ef39f5989d0dd2805d085da0bd3db3
Dan Grebb <git@dgrebb.com>
2024-04-19 09:54:38 -0400

    chore(app): comment and reduce testing logs

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -2,15 +2,15 @@
 import { writable } from 'svelte/store';
 
 type Item = {
-	id: string;
-	content: string;
+  id: string;
+  content: string;
 };
 
 // Our store will record an object containing an array of
 // items produced by the websocket.
 type State = {
-	items: Array<Item>;
-	error?: string;
+  items: Array<Item>;
+  error?: string;
 };
 
 // That's it;  state is now usable!  Components can subscribe
@@ -18,7 +18,7 @@ type State = {
 //
 // Note that this is a singleton.
 export const state = writable<State>({
-	items: []
+  items: [],
 });
 
 // We also want to connect to websockets.  Svelte does
@@ -26,29 +26,29 @@ export const state = writable<State>({
 // we will export a function that can be called by our root
 // component after mounting to connnect
 export const connect = (socketURL: string) => {
-	const ws = new WebSocket(`wss://${socketURL}`);
-	if (!ws) {
-		// Store an error in our state.  The function will be
-		// called with the current state;  this only adds the
-		// error.
-		state.update((s: State) => {
-			return { ...s, error: 'Unable to connect' };
-		});
-		return;
-	}
+  const ws = new WebSocket(`wss://${socketURL}`);
+  if (!ws) {
+    // Store an error in our state.  The function will be
+    // called with the current state;  this only adds the
+    // error.
+    state.update((s: State) => {
+      return { ...s, error: 'Unable to connect' };
+    });
+    return;
+  }
 
-	ws.addEventListener('open', () => {
-		// TODO: Set up ping/pong, etc.
-	});
+  ws.addEventListener('open', () => {
+    // TODO: Set up ping/pong, etc.
+  });
 
-	ws.addEventListener('message', (message: { data: string }) => {
-		const data: Item = JSON.parse(message.data);
-		// Mutate state by prepending the new data to the array.
-		state.update((state) => ({ ...state, items: [data].concat(state.items) }));
-	});
+  ws.addEventListener('message', (message: { data: string }) => {
+    const data: Item = JSON.parse(message.data);
+    // Mutate state by prepending the new data to the array.
+    state.update((state) => ({ ...state, items: [data].concat(state.items) }));
+  });
 
-	ws.addEventListener('close', (_message: unknown) => {
-		// TODO: Handle close
-		console.log(_message);
-	});
+  ws.addEventListener('close', (_message: unknown) => {
+    // TODO: Handle close
+    // console.log(_message);
+  });
 };
