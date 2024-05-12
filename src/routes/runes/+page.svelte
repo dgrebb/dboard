6fdc84e62a331c137744b017b47dcf933a4b69b6
Dan Grebb <git@dgrebb.com>
2024-05-11 21:35:13 -0400

    wip(app): initial svelte 5 rune implementation

---
+++ src/routes/runes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { getContext, setContext } from 'svelte';
+  import createWeatherInstance from '$lib/stores/weather.svelte';
+  import Component from './component.svelte';
+
+  let weather = createWeatherInstance({ temp: 44.4 });
+
+  const data = {
+    temp: 50.23,
+  };
+
+  function handleSetWeather() {
+    weather = data;
+  }
+</script>
+
+<button on:click={handleSetWeather}>Set Weather</button>
+<p>{weather.temp} asd fasdf as</p>
+<Component {weather} />
+
+<style>
+  /* your styles go here */
+</style>
