ec1d7fc9de0623a8ab4c40f5c4518082228cbd4a
Dan Grebb <git@dgrebb.com>
2024-05-17 22:27:47 -0400

    refactor(weather): add weather icon map and state

--- src/lib/components/widgets/CurrentWeather/WeatherIcon.svelte
+++ src/lib/components/widgets/CurrentWeather/WeatherIcon.svelte
@@ -1,172 +1,22 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import Icon from '@iconify/svelte';
+  import mapWeatherIcon from '../Weather/iconMap';
 
-  interface IconMap {
-    [index: number]: IconProps;
-  }
-
-  type IconProps = {
-    icon: string;
-    color: string;
-    name: string;
+  type Props = {
+    weatherCode: number;
+    isDay: number;
   };
 
-  export let weatherCode: number = 0;
-  export let isDay: number = 1;
+  let { weatherCode, isDay }: Props = $props();
+  let icon = $state('data-sunburst-24-filled');
+  let color = $state('#85aba0');
+  let name = $state('Loading');
 
   // console.log('🚀 ~ weatherCode:', weatherCode);
 
-  $: iconType = weatherCode;
-  $: icon = 'data-sunburst-24-filled';
-  $: color = '#85aba0';
-  $: name = 'Loading';
-
-  const lightYellowGray = '#fbf1de';
-  const brightGreen = '#37cc37';
-  const brightYellow = '#ccc737';
-  const paleGreen = '#37cc7f';
-  const paleYellow = '#e5e5e5';
-  const lightBlue = '#8882cf';
-  const darkBlue = '#55508f';
-  const darkBlueGray = '#7347a8';
-  const paleBlueGreenGray = '#85aba0';
-  const moonSprinkle = 'rgb(162, 207, 234)';
-  const daySprinkle = 'rgb(104, 70, 240)';
-
-  const dayIconMap: IconMap = {
-    0: {
-      icon: 'fluent:weather-sunny-20-filled',
-      color: brightYellow,
-      name: 'Clear',
-    },
-    1: {
-      icon: 'material-symbols-light:partly-cloudy-day',
-      color: paleGreen,
-      name: 'Mainly clear',
-    },
-    2: {
-      icon: 'fluent:weather-partly-cloudy-day-20-filled',
-      color: paleBlueGreenGray,
-      name: 'Partly Cloudy',
-    },
-    3: {
-      icon: 'fluent:weather-cloudy-20-filled',
-      color: '#62788d',
-      name: 'Overcast',
-    },
-    45: {
-      icon: 'fluent:weather-fog-20-filled',
-      color: paleBlueGreenGray,
-      name: 'Fog',
-    },
-    48: {
-      icon: 'fluent:weather-fog-20-filled',
-      color: paleBlueGreenGray,
-      name: 'Rime Fog',
-    },
-    51: {
-      icon: 'fluent:weather-drizzle-20-filled',
-      color: daySprinkle,
-      name: 'Light Drizzle',
-    },
-    53: {
-      icon: 'fluent:weather-drizzle-20-filled',
-      color: daySprinkle,
-      name: 'Moderate Drizzle',
-    },
-    55: {
-      icon: 'fluent:weather-drizzle-20-filled',
-      color: daySprinkle,
-      name: 'Dense Drizzle',
-    },
-    61: {
-      icon: 'fluent:weather-rain-20-filled',
-      color: lightBlue,
-      name: 'Light Rain',
-    },
-    63: {
-      icon: 'fluent:weather-rain-20-filled',
-      color: lightBlue,
-      name: 'Rain',
-    },
-    65: {
-      icon: 'fluent:weather-rain-20-filled',
-      color: lightBlue,
-      name: 'Heavy Rain',
-    },
-  };
-
-  const nightIconMap: IconMap = {
-    0: {
-      icon: 'fluent:weather-moon-20-filled',
-      color: paleYellow,
-      name: 'Clear',
-    },
-    1: {
-      icon: 'fluent:weather-partly-cloudy-night-20-filled',
-      color: lightYellowGray,
-      name: 'Mainly clear',
-    },
-    2: {
-      icon: 'fluent:weather-partly-cloudy-night-20-filled',
-      color: paleBlueGreenGray,
-      name: 'Partly Cloudy',
-    },
-    3: {
-      icon: 'fluent:weather-cloudy-20-filled',
-      color: paleBlueGreenGray,
-      name: 'Overcast',
-    },
-    45: {
-      icon: 'fluent:weather-fog-20-filled',
-      color: paleBlueGreenGray,
-      name: 'Fog',
-    },
-    48: {
-      icon: 'fluent:weather-fog-20-filled',
-      color: paleBlueGreenGray,
-      name: 'Rime Fog',
-    },
-    51: {
-      icon: 'fluent:weather-drizzle-20-filled',
-      color: moonSprinkle,
-      name: 'Light Drizzle',
-    },
-    53: {
-      icon: 'fluent:weather-drizzle-20-filled',
-      color: moonSprinkle,
-      name: 'Moderate Drizzle',
-    },
-    55: {
-      icon: 'fluent:weather-drizzle-20-filled',
-      color: moonSprinkle,
-      name: 'Dense Drizzle',
-    },
-    61: {
-      icon: 'fluent:weather-rain-20-filled',
-      color: darkBlue,
-      name: 'Light Rain',
-    },
-    63: {
-      icon: 'fluent:weather-rain-20-filled',
-      color: darkBlue,
-      name: 'Rain',
-    },
-    65: {
-      icon: 'fluent:weather-rain-20-filled',
-      color: darkBlue,
-      name: 'Heavy Rain',
-    },
-  };
-
   onMount(() => {
-    icon =
-      isDay === 1 ? dayIconMap[iconType].icon : nightIconMap[iconType].icon;
-    color =
-      isDay === 1 ? dayIconMap[iconType].color : nightIconMap[iconType].color;
-    name =
-      isDay === 1 ? dayIconMap[iconType].name : nightIconMap[iconType].name;
+    ({ icon, color, name } = mapWeatherIcon(weatherCode, isDay));
   });
 </script>
 
@@ -176,6 +26,6 @@
     <Icon {icon} {color} width={200} class="self-end" />
   {:else}
     <Icon icon="meteocons:not-available-fill" width={200} />
-    <p>{iconType}</p>
+    <p>{weatherCode}</p>
   {/if}
 </div>
