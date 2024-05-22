90b2f9ffa71f6617f2edc181471a0cae7eaa5eac
Dan Grebb <git@dgrebb.com>
2024-05-21 22:09:47 -0400

    feat(weather): add high and low to forecast

--- src/lib/stores/stores.svelte.ts
+++ src/lib/stores/stores.svelte.ts
@@ -15,6 +15,8 @@ export type WeatherType = {
   daily: {
     weather_code: string[];
     time: string[];
+    apparent_temperature_max: number[];
+    apparent_temperature_min: number[];
   };
 };
 
@@ -32,7 +34,7 @@ export const createWeather = function createWeather() {
       redirect: 'follow',
     };
     await fetch(
-      `${host}/api/v2/weather/forecast?latitude=${latitude}&longitude=${longitude}&current=apparent_temperature,wind_speed_10m,wind_direction_10m,wind_gusts_10m,cloud_cover,apparent_temperature,is_day,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&daily=sunrise,sunset,weather_code&timezone=America%2FNew_York`,
+      `${host}/api/v2/weather/forecast?latitude=${latitude}&longitude=${longitude}&current=apparent_temperature,wind_speed_10m,wind_direction_10m,wind_gusts_10m,cloud_cover,apparent_temperature,is_day,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&daily=sunrise,sunset,weather_code,apparent_temperature_max,apparent_temperature_min&timezone=America%2FNew_York`,
       requestOptions
     )
       .then(function (res) {
