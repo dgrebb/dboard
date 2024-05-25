d4017dd005a31777f3b4ca51d2498193865da748
Dan Grebb <git@dgrebb.com>
2024-05-25 15:28:10 -0600

    wip(app): add background store

---
+++ src/lib/stores/background.svelte.ts
@@ -0,0 +1,18 @@
+// TODO: Extract background functionality into store
+
+// const body = document.body;
+// let bgColor: string = body.style.getPropertyValue('--bgColor');
+// const { sunrise, sunset } weather.current;
+
+// const rise = new Date(sunrise).getTime();
+// const set = new Date(sunset).getTime();
+// const currentTime: number = new Date().getTime();
+// const gradientColor: string = calculateBackgroundColorGradient(
+//   currentTime,
+//   rise,
+//   set
+// );
+// bgColor = await calculateBaseBackgroundColorGradient(currentTime, rise, set);
+// body.style.setProperty('--gradient', gradientColor);
+// body.style.setProperty('--bgColor', bgColor);
+// body.classList.toggle('ready', true);
