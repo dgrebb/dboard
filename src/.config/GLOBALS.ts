d319bb23834c5970bf68b9485f91c30a8c0adc59
Dan Grebb <git@dgrebb.com>
2024-05-25 15:29:47 -0600

    revert(app): set GLOBAL lat/long back to string

--- src/.config/GLOBALS.ts
+++ src/.config/GLOBALS.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_TEMPO = 900000,
   WEATHER_API = 'https://api.open-meteo.com/v1',
   WEATHER_LAT_LONG = 'latitude=40.2415&longitude=-75.2838',
-  LATITUDE = 40.2415,
-  LONGITUDE = -75.2838,
+  LATITUDE = '40.2415',
+  LONGITUDE = '-75.2838',
   NIGHTSCOUT_API = 'https://glu.7ub3s.net/api/v1';
