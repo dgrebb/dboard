0f386d9d18b2789ec6bfe530f2150b667403dfbb
Dan Grebb <git@dgrebb.com>
2024-05-26 21:36:22 -0600

    chore(app): remove and comment logs

---
+++ src/lib/components/v2/widgets/Composer/New.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  const createNewWidget = function (type) {
+    console.log('type of widget: ', type);
+  };
+</script>
+
+<div
+  class="dboard__grid__item push-to-refresh current-weather relative transition-colors"
+>
+  <divclass="dboard__card border-none bg-transparent">
+    <button> ➕ </button>
+  </div>
+</div>
