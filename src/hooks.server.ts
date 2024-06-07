10c0132a4605c5950ddf65594c300410a7e7367a
Dan Grebb <git@dgrebb.com>
2024-06-07 19:12:45 -0400

    fix(hooks): move hooks to correct location

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,7 @@ import type { ExtendedGlobal } from '$lib/server/webSocketUtils';
 let wssInitialized = false;
 const startupWebsocketServer = () => {
   if (wssInitialized) return;
-  // console.log('[wss:kit] setup');
+  console.log('[wss:kit] setup');
   const wss = (globalThis as ExtendedGlobal)[GlobalThisWSS];
   if (wss !== undefined) {
     wss.on('connection', (ws, _request) => {
@@ -15,13 +15,10 @@ const startupWebsocketServer = () => {
       // const session = await getSessionFromCookie(request.headers.cookie || '');
       // if (!session) ws.close(1008, 'User not authenticated');
       // ws.userId = session.userId;
-      // console.log(`[wss:kit] client connected (${ws.socketId})`);
-      ws.send(
-        `Hello from SvelteKit ${new Date().toLocaleString()} (${ws.socketId})]`
-      );
+      console.log(`[wss:kit] client connected (${ws.socketId})`);
 
       ws.on('close', () => {
-        // console.log(`[wss:kit] client disconnected (${ws.socketId})`);
+        console.log(`[wss:kit] client disconnected (${ws.socketId})`);
       });
     });
     wssInitialized = true;
