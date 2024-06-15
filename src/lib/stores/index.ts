f93fbb725cc2ddb44ed1bf6bc2b67ae6dcffd502
Dan Grebb <git@dgrebb.com>
2024-06-14 23:39:01 -0400

    wip(nightscout): separate nightscout store (#30)

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,5 +1,5 @@
 export { createWeather, type WeatherType } from './weather.svelte';
 export { background } from './background.svelte';
-export { nightscout } from './nightscout.svelte';
+export { createNightScoutWidget } from './nightscout.svelte';
 export { streams } from './streams.svelte';
 export { createWidget } from './widget.svelte';
