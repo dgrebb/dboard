2485bb52465b3386a6c61fe1f9df3787bf975be4
Dan Grebb <git@dgrebb.com>
2024-06-16 00:56:41 -0400

    fix(nightscout): update Graph component to reload options when values change

--- src/lib/widgets/NightScout/NightScoutGraph.svelte
+++ src/lib/widgets/NightScout/NightScoutGraph.svelte
@@ -12,65 +12,70 @@
 
   let { maxMeasurable, series, mainColor, backgroundColor }: Props = $props();
 
+  let myChart: echarts.ECharts;
+
+  let option: EChartOption = $state({
+    grid: {
+      left: '0',
+      right: '0',
+      top: 0,
+      bottom: 0,
+      show: false,
+      containLabel: true,
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      show: false,
+      axisLabel: {
+        show: false,
+        inside: true,
+      },
+    },
+    yAxis: {
+      max: maxMeasurable,
+      type: 'value',
+      show: false,
+      boundaryGap: false,
+      axisLabel: {
+        show: false,
+        inside: true,
+      },
+    },
+    series: [
+      {
+        data: series,
+        type: 'line',
+        lineStyle: {
+          color: backgroundColor,
+          opacity: 0.8,
+        },
+        itemStyle: {
+          show: false,
+          opacity: 0,
+        },
+        areaStyle: {
+          color: backgroundColor,
+          opacity: 0.5,
+        },
+      },
+    ],
+    backgroundColor: 'transparent',
+  });
+
   onMount(() => {
     var chartDom = document.getElementById('main');
-    var myChart = echarts.init(chartDom, 'dark');
-    var option: EChartOption;
-
-    option = {
-      grid: {
-        left: '0',
-        right: '0',
-        top: 0,
-        bottom: 0,
-        show: false,
-        containLabel: true,
-      },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        show: false,
-        axisLabel: {
-          show: false,
-          inside: true,
-        },
-      },
-      yAxis: {
-        max: maxMeasurable,
-        type: 'value',
-        show: false,
-        boundaryGap: false,
-        axisLabel: {
-          show: false,
-          inside: true,
-        },
-      },
-      series: [
-        {
-          data: series,
-          type: 'line',
-          lineStyle: {
-            color: backgroundColor,
-            opacity: 0.8,
-          },
-          itemStyle: {
-            show: false,
-            opacity: 0,
-          },
-          areaStyle: {
-            color: backgroundColor,
-            opacity: 0.5,
-          },
-        },
-      ],
-      backgroundColor: 'transparent',
-    };
+    myChart = echarts.init(chartDom, 'dark');
 
     option && myChart.setOption(option);
 
     window.addEventListener('resize', function () {
       myChart.resize();
     });
+  });
+
+  $effect(() => {
+    myChart.setOption(option);
   });
 </script>
 
