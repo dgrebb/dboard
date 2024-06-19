a009a236304e3c52598fca9552193b49c28be8de
Dan Grebb <git@dgrebb.com>
2024-06-19 18:38:47 -0400

    wip(weather): add cloud_cover to API params

--- src/routes/api/v1/weather/+server.ts
+++ src/routes/api/v1/weather/+server.ts
@@ -32,7 +32,7 @@ async function fetchWeather() {
     redirect: 'follow',
   };
   const weather: WeatherData = await fetch(
-    `${WEATHER_API}/forecast?${WEATHER_LAT_LONG}&current=apparent_temperature,temperature_2m,wind_speed_10m,wind_direction_10m,wind_gusts_10m,cloud_cover,temperature_2m,apparent_temperature,is_day,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&daily=sunrise,sunset&timezone=America%2FNew_York`,
+    `${WEATHER_API}/forecast?${WEATHER_LAT_LONG}&current=apparent_temperature,temperature_2m,wind_speed_10m,wind_direction_10m,wind_gusts_10m,cloud_cover,is_day,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&daily=sunrise,sunset&timezone=America%2FNew_York`,
     requestOptions
   )
     .then((response) => response.json())
