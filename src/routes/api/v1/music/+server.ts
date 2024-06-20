867bca45dcc0f690de064a0f75480a73e3c17696
Dan Grebb <git@dgrebb.com>
2024-06-20 00:53:52 -0400

    wip(music): remove unused setTempo function in v1 music API

--- src/routes/api/v1/music/+server.ts
+++ src/routes/api/v1/music/+server.ts
@@ -4,37 +4,6 @@ import type { RequestHandler } from './$types';
 
 export const prerender = false;
 
-function setTempo(fetch, locals, url, time) {
-  let trackTitle = '';
-  setInterval(async function () {
-    const response = await fetchTrackInfo(fetch, url);
-    const newTrackTitle = response.title;
-    console.log('🚀 ~ setTempo ~ trackTitle:', trackTitle);
-    console.log('🚀 ~ newTrackTitle:', newTrackTitle);
-    console.log(`${trackTitle} is possibly6 the same as ${newTrackTitle}`);
-    console.log(
-      '🚀 ~ trackTitle != newTrackTitle:',
-      trackTitle != newTrackTitle
-    );
-
-    if (trackTitle != newTrackTitle && locals.wss) {
-      trackTitle = newTrackTitle;
-      console.log('🚀 ~ trackTitle:', trackTitle);
-      locals.wss.clients.forEach((client) => {
-        if (client.readyState === 1) {
-          // client.send(`Weather update — at ${new Date().toLocaleString()}`);
-          client.send(
-            JSON.stringify({
-              success: true,
-              ...response,
-            })
-          );
-        }
-      });
-    }
-  }, time);
-}
-
 async function fetchTrackInfo(fetch, url) {
   const music = await fetch(`${url.origin}/music.json`);
   const response = JSON.parse(await music.text());
