507da43f9b610c00cf27bbf5f12fa1fde57620a0
Dan Grebb <git@dgrebb.com>
2024-06-20 06:05:07 -0400

    wip(nightscout): move nightscoutDirectionIcon map helper

---
+++ src/lib/_helpers/directionIconMap.ts
@@ -0,0 +1,27 @@
+import { isNightScoutData, type NightScoutData } from '../types';
+
+export const mapNightScoutDirectionIcon = (
+  data: NightScoutData | false = false
+): string => {
+  let icon = 'iconamoon:cloud-error-light';
+  if (isNightScoutData(data)) {
+    switch (data[0].direction) {
+      case 'Up':
+        icon = 'ph:trend-up-light';
+        break;
+      case 'FortyFiveUp':
+        icon = 'ph:arrow-bend-right-up-light';
+        break;
+      case 'Flat':
+        icon = 'material-symbols-light:trending-flat';
+        break;
+      case 'Down':
+        icon = 'ph:trend-down-light';
+        break;
+      case 'FortyFiveDown':
+        icon = 'ph:arrow-bend-right-down-light';
+        break;
+    }
+  }
+  return icon;
+};
