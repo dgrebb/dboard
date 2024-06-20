a593217a8ae0730377450a7752444216395ba189
Dan Grebb <git@dgrebb.com>
2024-06-20 06:06:33 -0400

    wip(nightscout): move nightscout types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -136,6 +136,20 @@ export type NightScoutReading = {
 
 export type NightScoutData = NightScoutReading[];
 
+export const isNightScoutData = (
+  data: WidgetData['data']
+): data is NightScoutReading[] => {
+  return (
+    Array.isArray(data) &&
+    data.every(
+      (entry) =>
+        typeof entry === 'object' &&
+        'sgv' in entry &&
+        typeof entry.sgv === 'number'
+    )
+  );
+};
+
 export type PossibleWidgetData =
   | NightScoutData
   | CurrentWeather
