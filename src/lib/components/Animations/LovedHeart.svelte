9375787790b2a284d4a8d0f80e81120ff9b3f95c
Dan Grebb <git@dgrebb.com>
2024-06-19 20:32:50 -0400

    feat(music): add loved animation, transitions, and component (#31)

---
+++ src/lib/components/Animations/LovedHeart.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+
+  type Props = {
+    loved: boolean;
+    size: number;
+  };
+
+  function bounceIn(node: Node, { delay = 0, duration = 400 } = {}) {
+    return {
+      delay,
+      duration,
+      css: (t: number) => {
+        const scale = t;
+        return `
+          transform: scale(${scale});
+          opacity: ${t};
+        `;
+      },
+    };
+  }
+
+  function bounceOut(node: Node, { delay = 0, duration = 400 } = {}) {
+    return {
+      delay,
+      duration,
+      css: (t: number) => {
+        const scale = 1 - t;
+        return `
+          transform: scale(${scale});
+          opacity: ${1 - t};
+        `;
+      },
+    };
+  }
+
+  let { loved, size = 33 }: Props = $props();
+</script>
+
+{#if loved}
+  <div class="loved" in:bounceIn out:bounceOut>
+    <Icon
+      icon="emojione-monotone:heart-decoration"
+      width={size}
+      height={size}
+      class="loved-heart"
+    />
+  </div>
+{/if}
