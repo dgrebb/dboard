f43c3fa1a5d17ddf4125762501954bb420f017ac
Dan Grebb <git@dgrebb.com>
2024-06-21 08:05:15 -0400

    refactor(health): nightscout and healthStore improvements

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,5 +1,6 @@
-export { createWeather, type WeatherType } from './weather.svelte';
+export { createWeather } from './weather.svelte';
 export { background } from './background.svelte';
 export { streams } from './streams.svelte';
 export { createWidget } from './widget.svelte';
-export { createHomeState, homeState } from './home.svelte';
+export { createHomeStore, homeState } from './home.svelte';
+export { createHealthStore, healthState } from './health.svelte';
