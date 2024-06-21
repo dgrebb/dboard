7ce6558d79bcd94cf1677dfaf10d9e1f6776ff5e
Dan Grebb <git@dgrebb.com>
2024-06-21 10:50:26 -0400

    refactor(septa): move and rename NextToArrive widget

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,17 @@
 <script lang="ts">
-  import settings from '$root/.config/settings.json';
-  import { DEFAULT_TEMPO, LATITUDE, LONGITUDE } from '$root/.config/GLOBALS';
-  import Board from './(layouts)/Board.svelte';
-  import { onMount } from 'svelte';
   import time from '$lib/stores/time';
-  import type { DBoardItem } from '$root/lib/types';
+  import { DEFAULT_TEMPO, LATITUDE, LONGITUDE } from '$root/.config/GLOBALS';
+  import settings from '$root/.config/settings.json';
   import updateBackgroundColorGradient from '$root/lib/layout/background';
-  import NewWidget from '$components/Composer/NewWidget.svelte';
+  import SeptaNextToArrive from '$widgets/Septa/NextToArrive/NextToArrive.svelte';
   import NightScout from '$widgets/NightScout/NightScout.svelte';
   import NowPlaying from '$widgets/NowPlaying/NowPlaying.svelte';
+  import { onMount } from 'svelte';
+  import Board from './(layouts)/Board.svelte';
+  import { isSteptaNextToArriveDataArray } from '$root/lib/types';
   let mounted = $state(false);
   let refreshInterval = DEFAULT_TEMPO;
-  // let seconds = $state(0);
-  let schedule = $state();
-
-  // let items: DBoardItem[] | undefined = $state();
-  // const nightscoutData = async () => {
-  //   const res = await fetch('/api/v1/nightscout');
-  //   const data = await res.json();
-  //   const { nightscout } = data;
-  //   items = nightscout.items;
-  // };
+  let schedule: SeptaNextToArrive[] | boolean = $state(false);
 
   const fetchSeptaNextToArrive = async () => {
     const data = await fetch('/api/v1/septa', { method: 'GET' })
@@ -34,18 +25,15 @@
   let components: { [key: string]: any } = {};
 
   onMount(async () => {
-    // nightscoutData();
     updateBackgroundColorGradient(LATITUDE, LONGITUDE);
     fetchSeptaNextToArrive();
     setInterval(async () => {
-      // nightscoutData();
       fetchSeptaNextToArrive();
       updateBackgroundColorGradient(LATITUDE, LONGITUDE);
       // seconds = 0;
     }, refreshInterval);
 
     setInterval(() => {
-      // seconds++;
       let now = new Date();
       $time = now.getHours() * 60 + now.getMinutes();
     }, 1000);
@@ -73,15 +61,9 @@
     <NowPlaying />
   </Board>
 
-  <!-- <Board>
-    <div class="dboard__grid__item control-widget relative">
-      <div class="dboard__card">
-        <h1
-          class="flex h-full flex-col items-center justify-center text-9xl text-orange-500"
-        >
-          {seconds}
-        </h1>
-      </div>
-    </div>
-  </Board> -->
+  <Board>
+    {#if typeof schedule !== 'boolean' && isSteptaNextToArriveDataArray(schedule)}
+      <SeptaNextToArrive {schedule} />
+    {/if}
+  </Board>
 {/if}
