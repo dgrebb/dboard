e5fed0b03d6ffcaaeded038bfea294c28ad9f6fe
Dan Grebb <git@dgrebb.com>
2024-06-30 03:14:19 -0400

    wip(music): clean up CSS

--- src/lib/widgets/NowPlaying/PlayHead.svelte
+++ src/lib/widgets/NowPlaying/PlayHead.svelte
@@ -23,44 +23,7 @@
 </div>
 
 <style lang="postcss">
-  .track-progress {
-    margin: 1rem 0;
-    position: relative;
-    overflow: hidden;
-    height: 1rem;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    border: thin white solid;
-    &::before {
-      background-color: white;
-      position: absolute;
-      z-index: 3;
-      height: 100%;
-      width: var(--percent);
-      content: '';
-      transition: width 2s linear;
-    }
-    &::before,
-    .playhead {
-      top: 0;
-    }
-    .playhead {
-      transition: left 2s linear;
-      position: relative;
-      left: var(--percent);
-      height: 1rem;
-      width: 0.25rem;
-      mix-blend-mode: normal;
-      /* background-color: white; */
-      mix-blend-mode: darken;
-      background-color: white;
-      &:hover,
-      &:focus,
-      &:active {
-        background-color: black;
-        width: 0.75rem;
-      }
-    }
+  .playhead {
+    left: var(--percent);
   }
 </style>
