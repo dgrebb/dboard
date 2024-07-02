aa94b2680e461792edb4dfa0812950f67daeeb1b
Dan Grebb <git@dgrebb.com>
2024-07-02 13:51:54 -0400

    fix(weather): adjust conditional to parse globalIsDay during mount

--- src/lib/widgets/CurrentWeather/CurrentWeather.svelte
+++ src/lib/widgets/CurrentWeather/CurrentWeather.svelte
@@ -21,7 +21,7 @@
   let weather = createWeather();
   let current: WeatherData['current'] | undefined = $state(weather.current());
   let daily: WeatherData['daily'] | undefined = $state(weather.daily());
-  let globalIsDay: number | undefined = $state(homeState.globalIsDay());
+  let globalIsDay: number | undefined = $state(undefined);
 
   function handlePushing(e: MouseEvent) {
     if (e.button === 2) return;
@@ -38,6 +38,13 @@
 
   type Props = {
     settings: CurrentWeatherSettings;
+  };
+
+  const nightOverride = (isDay: number, localDay: number) => {
+    return localDay === 1 && isDay === 0;
+  };
+  const dayOverride = (isDay: number, localDay: number) => {
+    return localDay === 0 && isDay === 1;
   };
 
   onMount(async () => {
@@ -62,22 +69,22 @@
   $effect(() => {
     daily = weather.daily();
     current = weather.current();
+    if (globalIsDay === undefined) {
+      setTimeout(async () => {
+        globalIsDay = await homeState.globalIsDay();
+      }, 100);
+    }
     if (
       location.primary === true &&
       typeof daily === 'object' &&
       typeof current === 'object'
     ) {
-      if (globalIsDay === undefined) {
-        setTimeout(() => {
-          globalIsDay = current?.is_day;
-        }, 100);
-      }
       background.updateColor(current, daily);
     }
   });
 </script>
 
-{#key globalIsDay}
+{#if current !== undefined && globalIsDay}
   <div
     onmousedown={(e) => handlePushing(e)}
     onmouseup={(e) => handleUp(e)}
@@ -85,78 +92,76 @@
     role="button"
     style={`--mainColor: ${highlightColor}`}
     class="dboard__grid__item push-to-refresh current-weather relative"
-    class:night-override={globalIsDay === 1 && current?.is_day === 0}
-    class:day-override={globalIsDay === 0 && current?.is_day === 1}
+    class:night-override={nightOverride(current?.is_day, globalIsDay)}
+    class:day-override={dayOverride(current?.is_day, globalIsDay)}
   >
-    {#if current}
-      <div
-        class="dboard__card border-none bg-transparent"
-        class:pushing
-        class:refreshed
-        transition:fade
+    <div
+      class="dboard__card border-none bg-transparent"
+      class:pushing
+      class:refreshed
+      transition:fade
+    >
+      <h2
+        class="brightness-25 text-[var(--mainColor)] bg-blend-darken dark:saturate-200"
       >
-        <h2
-          class="brightness-25 text-[var(--mainColor)] bg-blend-darken dark:saturate-200"
-        >
-          {name}
-          {#key current.wind_direction_10m}
-            <span
-              in:blur={{ duration: 333, delay: 335 }}
-              out:blur={{ duration: 333, delay: 0 }}
-            >
-              <Icon
-                icon="ei:arrow-up"
-                class="current-weather__wind-direction brightness-25 inline-block mix-blend-darken dark:brightness-200"
-                width={27}
-                style={`transform: rotate(${(current.wind_direction_10m + 222) % 360}deg);`}
-                color={highlightColor}
-              />
-            </span>
-          {/key}
-        </h2>
-        {#key current.wind_speed_10m}
-          <div
-            class="current-weather__wind"
-            in:blur={{ duration: 333, delay: 335 }}
-            out:blur={{ duration: 333, delay: 0 }}
-          >
-            <p
-              class="brightness-25 text-sm text-[var(--mainColor)] dark:brightness-150"
-            >
-              {current.wind_speed_10m} ↝ {current.wind_gusts_10m}mph
-            </p>
-          </div>
-        {/key}
-
-        {#key current.weather_code}
+        {name}
+        {#key current.wind_direction_10m}
           <span
             in:blur={{ duration: 333, delay: 335 }}
             out:blur={{ duration: 333, delay: 0 }}
           >
-            <WeatherIcon
-              weatherCode={typeof current.weather_code === 'number'
-                ? current.weather_code
-                : 0}
-              isDay={current.is_day}
+            <Icon
+              icon="ei:arrow-up"
+              class="current-weather__wind-direction brightness-25 inline-block mix-blend-darken dark:brightness-200"
+              width={27}
+              style={`transform: rotate(${(current.wind_direction_10m + 222) % 360}deg);`}
+              color={highlightColor}
             />
           </span>
         {/key}
-        {#key current.temperature_2m}
-          <h1
-            class="dboard__card--value-lg current-temperature mt-auto justify-end text-9xl text-[var(--mainColor)] brightness-50 dark:brightness-150 dark:saturate-200"
-            in:blur={{ duration: 333, delay: 335 }}
-            out:blur={{ duration: 333, delay: 0 }}
+      </h2>
+      {#key current.wind_speed_10m}
+        <div
+          class="current-weather__wind"
+          in:blur={{ duration: 333, delay: 335 }}
+          out:blur={{ duration: 333, delay: 0 }}
+        >
+          <p
+            class="brightness-25 text-sm text-[var(--mainColor)] dark:brightness-150"
           >
-            {Math.round(current.temperature_2m)}<span
-              class="dboard__card__value-symbol text-[var(--mainColor)] brightness-125 dark:brightness-150"
-              >&deg;</span
-            >
-          </h1>
-        {/key}
-      </div>
-    {/if}
+            {current.wind_speed_10m} ↝ {current.wind_gusts_10m}mph
+          </p>
+        </div>
+      {/key}
+
+      {#key current.weather_code}
+        <span
+          in:blur={{ duration: 333, delay: 335 }}
+          out:blur={{ duration: 333, delay: 0 }}
+        >
+          <WeatherIcon
+            weatherCode={typeof current.weather_code === 'number'
+              ? current.weather_code
+              : 0}
+            isDay={current.is_day}
+          />
+        </span>
+      {/key}
+      {#key current.temperature_2m}
+        <h1
+          class="dboard__card--value-lg current-temperature mt-auto justify-end text-9xl text-[var(--mainColor)] brightness-50 dark:brightness-150 dark:saturate-200"
+          in:blur={{ duration: 333, delay: 335 }}
+          out:blur={{ duration: 333, delay: 0 }}
+        >
+          {Math.round(current.temperature_2m)}<span
+            class="dboard__card__value-symbol text-[var(--mainColor)] brightness-125 dark:brightness-150"
+            >&deg;</span
+          >
+        </h1>
+      {/key}
+    </div>
   </div>
-{/key}
+{/if}
 
 <style>
   .push-to-refresh {
