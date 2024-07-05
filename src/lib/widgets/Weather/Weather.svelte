01c5a37c053815c35df3ce1a2347d0a5f6711a22
Dan Grebb <git@dgrebb.com>
2024-07-05 04:59:55 -0400

    wip(weather): rename and move Weather compoent; adjust imports; make `settings` optional on widget settings object (#28)

---
+++ src/lib/widgets/Weather/Weather.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { CurrentWeatherData } from '$lib/types';
+  import { homeState } from '$lib/stores';
+  import '$widgets/weather/weather.css';
+
+  let temperature: CurrentWeatherData['temperature_2m'] = $state(
+    homeState.currentRoundTemperature()
+  );
+
+  $effect(() => {
+    temperature = homeState.currentRoundTemperature();
+  });
+</script>
+
+<div class="current-weather text-white">
+  <h1>{temperature}</h1>
+</div>
