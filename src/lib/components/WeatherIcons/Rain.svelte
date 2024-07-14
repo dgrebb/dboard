40b4c60dc1e475ac7be0c7b472ef98edc94f65c6
Dan Grebb <git@dgrebb.com>
2024-07-14 12:54:29 -0400

    style(weather): dedupelicate svg definition ids/refs (#108)

--- src/lib/components/WeatherIcons/Rain.svelte
+++ src/lib/components/WeatherIcons/Rain.svelte
@@ -5,7 +5,7 @@
 >
   <defs>
     <linearGradient
-      id="a"
+      id="Rain_a"
       x1="100"
       x2="233"
       y1="31"
@@ -16,7 +16,7 @@
       <stop offset="1" stop-color="#deeafb" />
     </linearGradient>
     <linearGradient
-      id="b"
+      id="Rain_b"
       x1="1381"
       x2="1399"
       y1="-1145"
@@ -28,8 +28,8 @@
       <stop offset="1" stop-color="#0950bc" />
     </linearGradient>
     <linearGradient
-      xlink:href="#b"
-      id="c"
+      xlink:href="#Rain_b"
+      id="Rain_c"
       x1="1437"
       x2="1455"
       y1="-1137"
@@ -37,33 +37,33 @@
       gradientTransform="rotate(-9 8010 8233)"
     />
     <linearGradient
-      xlink:href="#b"
-      id="d"
+      xlink:href="#Rain_b"
+      id="Rain_d"
       x1="1492"
       x2="1510"
       y1="-1129"
       y2="-1082"
       gradientTransform="rotate(-9 8017 8233)"
     />
-    <symbol id="e" viewBox="0 0 350 222">
+    <symbol id="Rain_e" viewBox="0 0 350 222">
       <path
-        fill="url(#a)"
+        fill="url(#Rain_a)"
         stroke="#e6effc"
         stroke-miterlimit="10"
         stroke-width="6"
         d="m291 107-2.5.1A84 84 0 00136 43a56 56 0 00-85 48 60 60 0 001 9 60 60 0 0012 121h224a56 56 0 000-112Z"
       />
     </symbol>
-    <symbol id="f" overflow="visible" viewBox="0 0 129 57">
+    <symbol id="Rain_f" overflow="visible" viewBox="0 0 129 57">
       <path
-        fill="url(#b)"
+        fill="url(#Rain_b)"
         stroke="#0a5ad4"
         stroke-miterlimit="10"
         d="M8 56a8 8 0 01-8-8v-40a8 8 0 0116 0v40a8 8 0 01-8 8Z"
         opacity="0"
       >
         <animateTransform
-          id="x1"
+          id="Rain_x1"
           additive="sum"
           attributeName="transform"
           begin="0s; x1.end+1s"
@@ -72,7 +72,7 @@
           values="0 -60; 0 60"
         />
         <animate
-          id="y1"
+          id="Rain_y1"
           attributeName="opacity"
           begin="0s; y1.end+1s"
           dur="1s"
@@ -81,14 +81,14 @@
         />
       </path>
       <path
-        fill="url(#c)"
+        fill="url(#Rain_c)"
         stroke="#0a5ad4"
         stroke-miterlimit="10"
         d="M64 56a8 8 0 01-8-8v-40a8 8 0 0116 0v40a8 8 0 01-8 8Z"
         opacity="0"
       >
         <animateTransform
-          id="x2"
+          id="Rain_x2"
           additive="sum"
           attributeName="transform"
           begin="1s; x2.end+1s"
@@ -97,7 +97,7 @@
           values="0 -60; 0 60"
         />
         <animate
-          id="y2"
+          id="Rain_y2"
           attributeName="opacity"
           begin="1s; y2.end+1s"
           dur="1s"
@@ -106,14 +106,14 @@
         />
       </path>
       <path
-        fill="url(#d)"
+        fill="url(#Rain_d)"
         stroke="#0a5ad4"
         stroke-miterlimit="10"
         d="M120 56a8 8 0 01-8-8v-40a8 8 0 0116 0v40a8 8 0 01-8 8Z"
         opacity="0"
       >
         <animateTransform
-          id="x3"
+          id="Rain_x3"
           additive="sum"
           attributeName="transform"
           begin="-1s; x3.end+1s"
@@ -122,7 +122,7 @@
           values="0 -60; 0 60"
         />
         <animate
-          id="y3"
+          id="Rain_y3"
           attributeName="opacity"
           begin="-1s; y3.end+1s"
           dur="1s"
@@ -132,9 +132,14 @@
       </path>
     </symbol>
   </defs>
-  <use xlink:href="#e" width="364" height="222" transform="translate(81 145)" />
   <use
-    xlink:href="#f"
+    xlink:href="#Rain_e"
+    width="364"
+    height="222"
+    transform="translate(81 145)"
+  />
+  <use
+    xlink:href="#Rain_f"
     width="129"
     height="57"
     transform="translate(191.5 343.5)"
