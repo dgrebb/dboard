c9c38d228726bcf56f1de40ed15b85c95b0316dc
Dan Grebb <git@dgrebb.com>
2024-07-20 23:07:15 -0400

    wip(calendar): add events API (#118)

---
+++ src/routes/api/static/calendar/+server.ts
@@ -0,0 +1,24 @@
+import type { CalendarEvent } from '$widgets/Schedule/Schedule.types';
+import type { RequestHandler } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
+
+/**
+ * Handles GET requests to fetch and filter event data.
+ *
+ * @param {Object} request - The request object.
+ * @param {Function} fetch - The fetch function to make network requests.
+ * @param {URL} url - The URL object representing the request URL.
+ * @returns {Promise<Response>} A JSON response containing filtered events.
+ */
+export const GET: RequestHandler = async ({ fetch, url }) => {
+  const eventRange = url.searchParams.get('range');
+  const source = await fetch(`/${eventRange}.json`);
+  const events: CalendarEvent[] = JSON.parse(await source.text());
+
+  // Filter out events with duration >= 120 that are of the "calendar": "Calendar" type
+  const filteredEvents = events.filter(
+    (event) => !(event.calendar === 'Calendar' && event.duration >= 120)
+  );
+
+  return json({ success: true, events: filteredEvents });
+};
