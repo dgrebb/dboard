e066bcc32305d5fa20b5a30010ce780ca2fb7da5
Dan Grebb <git@dgrebb.com>
2024-07-29 22:52:40 -0400

    wip(schedule): adding types, styles, and data for on/off display schedule (#128)

--- src/lib/stores/home.types.ts
+++ src/lib/stores/home.types.ts
@@ -7,11 +7,4 @@ export type HomeData = {
   location: LocationType;
   lights?: unknown;
   weather?: WeatherData;
-  time?: {
-    milliseconds: 0;
-    hours: 0;
-    minutes: 0;
-    seconds: 0;
-    date: 0;
-  };
 };
