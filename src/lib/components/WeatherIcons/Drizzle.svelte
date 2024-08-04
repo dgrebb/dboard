aee344a896c114dbf960eb9443bbf0a19d70b545
Dan Grebb <git@dgrebb.com>
2024-08-04 06:47:01 -0400

    feat(music): improve album gradient vibrancy and direction variance (#142) (#153)
    * style(weather): add NightCloudy and image asset
    
    * build(app): adjust awtch command to pass only changed files
    
    * feat(light): add `pre-dawn` and `isTwilight` calculations to background gradient math/css
    
    * feat(music): album art gradient randomization and vibrancy improvements (#142)
    
    * revert(weather): remove static weather icon images; add Drizzle icon

---
+++ src/lib/components/WeatherIcons/Drizzle.svelte
@@ -0,0 +1,170 @@
+<script lang="ts"></script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  viewBox="0 0 512 512"
+  ><defs
+    ><linearGradient
+      id="Drizzle_a"
+      x1="99.5"
+      x2="232.6"
+      y1="30.7"
+      y2="261.4"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#f3f7fe" /><stop
+        offset=".5"
+        stop-color="#f3f7fe"
+      /><stop offset="1" stop-color="#deeafb" /></linearGradient
+    ><linearGradient
+      id="Drizzle_b"
+      x1="34.7"
+      x2="119.2"
+      y1="18.6"
+      y2="165"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#86c3db" /><stop
+        offset=".5"
+        stop-color="#86c3db"
+      /><stop offset="1" stop-color="#5eafcf" /></linearGradient
+    ><linearGradient
+      id="Drizzle_c"
+      x1="1381.3"
+      x2="1399.5"
+      y1="-1144.7"
+      y2="-1097.4"
+      gradientTransform="rotate(-9 8002.567 8233.063)"
+      gradientUnits="userSpaceOnUse"
+      ><stop offset="0" stop-color="#0b65ed" /><stop
+        offset=".5"
+        stop-color="#0a5ad4"
+      /><stop offset="1" stop-color="#0950bc" /></linearGradient
+    ><linearGradient
+      xlink:href="#Drizzlec"
+      id="Drizzle_d"
+      x1="1436.7"
+      x2="1454.9"
+      y1="-1137"
+      y2="-1089.7"
+      gradientTransform="rotate(-9 8009.537 8233.037)"
+    /><linearGradient
+      xlink:href="#Drizzlec"
+      id="Drizzle_e"
+      x1="1492.1"
+      x2="1510.3"
+      y1="-1129.3"
+      y2="-1082.1"
+      gradientTransform="rotate(-9 8016.566 8233.078)"
+    /><symbol id="Drizzle_g" viewBox="0 0 172 172"
+      ><path
+        fill="url(#Drizzle_b)"
+        stroke="#72b9d5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="4"
+        d="M160.6 107.4a84.8 84.8 0 01-85.4-84.3A83.3 83.3 0 0178 2 84.7 84.7 0 002 85.7 84.8 84.8 0 0087.4 170a85.2 85.2 0 0082.6-63.1 88 88 0 01-9.4.5Z"
+        ><animateTransform
+          additive="sum"
+          attributeName="transform"
+          dur="6s"
+          repeatCount="indefinite"
+          type="rotate"
+          values="-15 86 86; 9 86 86; -15 86 86"
+        /></path
+      ></symbol
+    ><symbol id="Drizzle_h" viewBox="0 0 350 222"
+      ><path
+        fill="url(#Drizzle_a)"
+        stroke="#e6effc"
+        stroke-miterlimit="10"
+        stroke-width="6"
+        d="m291 107-2.5.1A83.9 83.9 0 00135.6 43 56 56 0 0051 91a56.6 56.6 0 00.8 9A60 60 0 0063 219l4-.2v.2h224a56 56 0 000-112Z"
+      /></symbol
+    ><symbol id="Drizzle_f" viewBox="0 0 351 246"
+      ><use xlink:href="#Drizzleg" width="172" height="172" /><use
+        xlink:href="#Drizzleh"
+        width="350"
+        height="222"
+        transform="translate(1 24)"
+      /></symbol
+    ><symbol id="Drizzle_i" overflow="visible" viewBox="0 0 129 57"
+      ><path
+        fill="url(#Drizzle_c)"
+        stroke="#0a5ad4"
+        stroke-miterlimit="10"
+        d="M8.5 56.5a8 8 0 01-8-8v-40a8 8 0 0116 0v40a8 8 0 01-8 8Z"
+        opacity="0"
+        ><animateTransform
+          id="Drizzle_x1"
+          additive="sum"
+          attributeName="transform"
+          begin="0s; Drizzle_x1.end+.33s"
+          dur=".67s"
+          type="translate"
+          values="0 -60; 0 60"
+        /><animate
+          id="Drizzle_y1"
+          attributeName="opacity"
+          begin="0s; Drizzle_y1.end+.33s"
+          dur=".67s"
+          keyTimes="0; .25; 1"
+          values="0; 1; 0"
+        /></path
+      ><path
+        fill="url(#Drizzle_d)"
+        stroke="#0a5ad4"
+        stroke-miterlimit="10"
+        d="M64.5 56.5a8 8 0 01-8-8v-40a8 8 0 0116 0v40a8 8 0 01-8 8Z"
+        opacity="0"
+        ><animateTransform
+          id="Drizzle_x2"
+          additive="sum"
+          attributeName="transform"
+          begin=".33s; Drizzle_x2.end+.33s"
+          dur=".67s"
+          type="translate"
+          values="0 -60; 0 60"
+        /><animate
+          id="Drizzle_y2"
+          attributeName="opacity"
+          begin=".33s; Drizzle_y2.end+.33s"
+          dur=".67s"
+          keyTimes="0; .25; 1"
+          values="0; 1; 0"
+        /></path
+      ><path
+        fill="url(#Drizzle_e)"
+        stroke="#0a5ad4"
+        stroke-miterlimit="10"
+        d="M120.5 56.5a8 8 0 01-8-8v-40a8 8 0 0116 0v40a8 8 0 01-8 8Z"
+        opacity="0"
+        ><animateTransform
+          id="Drizzle_x3"
+          additive="sum"
+          attributeName="transform"
+          begin="-.33s; Drizzle_x3.end+.33s"
+          dur=".67s"
+          type="translate"
+          values="0 -60; 0 60"
+        /><animate
+          id="Drizzle_y3"
+          attributeName="opacity"
+          begin="-.33s; Drizzle_y3.end+.33s"
+          dur=".67s"
+          keyTimes="0; .25; 1"
+          values="0; 1; 0"
+        /></path
+      ></symbol
+    ></defs
+  ><use
+    xlink:href="#Drizzlef"
+    width="351"
+    height="246"
+    transform="translate(80 121)"
+  /><use
+    xlink:href="#Drizzlei"
+    width="129"
+    height="57"
+    transform="translate(191.5 343.5)"
+  /></svg
+>
