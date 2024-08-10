6b98323c962f81c4d545d84fe312611bf45314bf
Dan Grebb <git@dgrebb.com>
2024-08-10 13:58:54 -0400

    style(weather): add nightpartlycloudy and fix nightcloudy

--- src/lib/components/WeatherIcons/NIghtCloudy.svelte
+++ src/lib/components/WeatherIcons/NIghtCloudy.svelte
@@ -1,4 +1,43 @@
 <script lang="ts">
 </script>
 
-<img src="/weather/NightCloudy.webp" alt="Night Cloudy" />
+<svg width="200" height="200" viewBox="0 0 100 100"
+  ><g style="transform-origin:50% 50%;transform:rotate(0deg) scale(.8)"
+    ><path
+      fill="#b19fc6"
+      stroke="#323334"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="3.5"
+      d="M85.52 46.14s0 0 0 0c-.15-7.59-6.34-13.69-13.96-13.69-5.9 0-10.93 3.66-12.98 8.82-.99-.26-2.02-.4-3.08-.4-6.69 0-12.11 5.42-12.11 12.11 0 6.69 5.42 12.11 12.11 12.11h30.02a9.474 9.474 0 1 0 0-18.95z"
+      style="transform-box:view-box;opacity:1;transform-origin:50px 50px;transform:matrix(1,0,0,1,3,0);animation:animate 8.33333s linear -6.25s infinite"
+    /><path
+      fill="#8c8c95"
+      stroke="#323334"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="3.5"
+      d="M66.95 49.81c1.08-2.39 1.68-5.03 1.68-7.82 0-10.52-8.53-19.05-19.05-19.05-7.83 0-14.56 4.73-17.49 11.49-1.77-.9-3.76-1.42-5.88-1.42-7.15 0-12.94 5.79-12.94 12.94 0 1.48.26 2.89.72 4.22A12.909 12.909 0 0 0 5 62.48c0 7.03 5.6 12.73 12.58 12.92v.02h46.73c7.15 0 12.94-5.79 12.94-12.94.01-6.24-4.42-11.45-10.3-12.67z"
+      style="transform-box:view-box;opacity:1;transform-origin:50px 50px;transform:matrix(1,0,0,1,3,0);animation:animate 8.33333s linear -8.33333s infinite"
+    /></g
+  ><style type="text/css">
+    @keyframes animate {
+      0% {
+        transform: translate(3px, 0) rotate(0deg) scale(1) skew(0deg);
+        opacity: 1;
+      }
+      26% {
+        transform: translate(-0.3px, 0) rotate(0deg);
+      }
+      50% {
+        transform: translate(-3px, 0) rotate(0deg);
+      }
+      76% {
+        transform: translate(0.3px, 0) rotate(0deg);
+      }
+      to {
+        transform: translate(3px, 0) rotate(0deg);
+      }
+    }
+  </style></svg
+>
---
+++ src/lib/components/WeatherIcons/NightPartlyCloudy.svelte
@@ -0,0 +1,42 @@
+<script lang="ts"></script>
+
+<svg width="200" height="200" viewBox="0 0 100 100"
+  ><g style="transform-origin:50% 50%;transform:rotate(0deg) scale(.85)"
+    ><path
+      fill="#fbfa98"
+      stroke="#0f3152"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1.12"
+      d="M83.56 46.46c-13.64 0-24.69-11.06-24.69-24.69 0-2.59.4-5.1 1.14-7.44-10 3.16-17.25 12.51-17.25 23.55 0 13.64 11.06 24.69 24.69 24.69 11.04 0 20.39-7.25 23.55-17.25-2.35.74-4.85 1.14-7.44 1.14z"
+      style="transform-box:view-box;opacity:1;transform-origin:50px 50px;transform:matrix(1,0,0,1,5,0);animation:animate 12.5s linear -9.375s infinite"
+    /><path
+      fill="#deebf7"
+      stroke="#0f3152"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="1.12"
+      d="M17.45 61.49c-.1-.78-.16-1.58-.16-2.39 0-10.1 8.19-18.3 18.3-18.3 7.73 0 14.33 4.79 17.01 11.56 1.53-.77 3.25-1.21 5.08-1.21 5.84 0 10.64 4.39 11.31 10.05 5.4 1.35 9.41 6.22 9.41 12.04 0 6.75-5.38 12.23-12.08 12.41H21.43C14.56 85.68 9 80.11 9 73.25c0-5.47 3.54-10.1 8.45-11.76z"
+      style="transform-box:view-box;opacity:1;transform-origin:50px 50px;transform:matrix(1,0,0,1,5,0);animation:animate 12.5s linear -12.5s infinite"
+    /></g
+  ><style type="text/css">
+    @keyframes animate {
+      0% {
+        transform: translate(5px, 0) rotate(0deg) scale(1) skew(0deg);
+        opacity: 1;
+      }
+      26% {
+        transform: translate(-0.5px, 0) rotate(0deg);
+      }
+      50% {
+        transform: translate(-5px, 0) rotate(0deg);
+      }
+      76% {
+        transform: translate(0.5px, 0) rotate(0deg);
+      }
+      to {
+        transform: translate(5px, 0) rotate(0deg);
+      }
+    }
+  </style></svg
+>
