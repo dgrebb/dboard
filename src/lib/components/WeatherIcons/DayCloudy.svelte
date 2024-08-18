0409269109e309bb2baba50098a47897f68a5926
Dan Grebb <git@dgrebb.com>
2024-08-18 05:02:05 -0400

    style(weather): weather icon updates; size decrease

---
+++ src/lib/components/WeatherIcons/DayCloudy.svelte
@@ -0,0 +1,177 @@
+<script lang="ts">
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xml:space="preserve"
+  id="Layer_1"
+  width="200"
+  height="200"
+  x="0"
+  y="0"
+  preserveAspectRatio="xMidYMid"
+  style="margin:initial;display:block;shape-rendering:auto;background:0 0"
+  version="1.1"
+  viewBox="0 0 100 100"
+  ><g
+    class="ldl-scale"
+    style="transform-origin:50% 50%0;transform:rotate(0deg) scale(.8)"
+    ><g class="ldl-ani"
+      ><g class="ldl-layer"
+        ><g
+          class="ldl-ani"
+          style="transform-box:view-box;opacity:1;transform-origin:50px 50px 0;transform:matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,10,0,0,1);animation:12.5s linear -9.375s infinite forwards animate"
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><circle
+              cx="44"
+              cy="44"
+              r="19.63"
+              fill="#FEE1C5"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              class="ldl-ani"
+              style="stroke-width:1.33px;fill:#fef1c5;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m30.61 11.66 2.94 7.1"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m11.66 30.61 7.1 2.94"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m11.66 57.39 7.1-2.94"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m30.61 76.34 2.94-7.1"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m57.39 76.34-2.94-7.1"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m76.34 57.39-7.1-2.94"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m76.34 30.61-7.1 2.94"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ><g class="ldl-layer" style="transform-box:view-box"
+            ><path
+              fill="none"
+              stroke="#323334"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-miterlimit="10"
+              stroke-width="3.5"
+              d="m57.39 11.66-2.94 7.1"
+              class="ldl-ani"
+              style="stroke-width:1.33px;stroke:#adaf71"
+            /></g
+          ></g
+        ></g
+      ><g
+        class="ldl-layer"
+        style="transform-box:view-box;opacity:1;transform-origin:50px 50px 0;transform:matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,10,0,0,1);animation:12.5s linear -12.5s infinite forwards animate"
+        ><path
+          fill="#F2F2F3"
+          stroke="#323334"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-miterlimit="10"
+          stroke-width="3.5"
+          d="M82.29 63.59c.1-.79.16-1.59.16-2.41 0-10.19-8.26-18.46-18.46-18.46-7.79 0-14.46 4.83-17.16 11.67-1.54-.77-3.28-1.22-5.13-1.22-5.89 0-10.74 4.43-11.41 10.14-5.45 1.36-9.49 6.28-9.49 12.15 0 6.81 5.43 12.33 12.19 12.52V88h45.27c6.92 0 12.54-5.61 12.54-12.54.01-5.51-3.56-10.19-8.51-11.87z"
+          class="ldl-ani"
+          style="stroke-width:1.33px;fill:#f2f2f3;stroke:#adaf71"
+        /></g
+      ></g
+    ></g
+  ><style>
+    @keyframes animate {
+      0% {
+        transform: translate(10px, 0) rotate(0deg) scale(1, 1) skew(0deg, 0deg);
+        opacity: 1;
+        animation-timing-function: cubic-bezier(0.59, 0.02, 0.79, 0.52);
+      }
+      26% {
+        transform: translate(-1px, 0) rotate(0deg);
+        animation-timing-function: cubic-bezier(0.19, 0.49, 0.42, 0.98);
+      }
+      50% {
+        transform: translate(-10px, 0) rotate(0deg);
+        animation-timing-function: cubic-bezier(0.59, 0.02, 0.79, 0.52);
+      }
+      76% {
+        transform: translate(1px, 0) rotate(0deg);
+        animation-timing-function: cubic-bezier(0.21, 0.48, 0.44, 0.93);
+      }
+      to {
+        transform: translate(10px, 0) rotate(0deg);
+        animation-timing-function: cubic-bezier(0.21, 0.48, 0.44, 0.93);
+      }
+    }
+  </style></svg
+>
