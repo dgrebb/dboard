0f732f30682d81f76856004b4281b7f98a362e36
Dan Grebb <git@dgrebb.com>
2024-08-26 17:58:54 -0400

    style(weather): increase icon size and fix/commit capitalization in `NIghtCloudy.svlete`

--- src/lib/components/WeatherIcons/NightCloudy.svelte
+++ src/lib/components/WeatherIcons/NightCloudy.svelte
@@ -19,7 +19,7 @@
     ><g class="ldl-ani"
       ><g
         class="ldl-layer"
-        style="transform-box:view-box;opacity:1;transform-origin:50px 50px 0;transform:matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,10,0,0,1);animation:14.2857s linear -10.7143s infinite forwards animate"
+        style="transform-box:view-box;opacity:1;transform-origin:50px 50px 0;transform:matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,10,0,0,1);animation:20s linear -15s infinite forwards animate"
         ><path
           fill="#FEE1C5"
           stroke="#323334"
@@ -29,11 +29,11 @@
           stroke-width="3.5"
           d="M83.56 46.46c-13.64 0-24.69-11.06-24.69-24.69 0-2.59.4-5.1 1.14-7.44-10 3.16-17.25 12.51-17.25 23.55 0 13.64 11.06 24.69 24.69 24.69 11.04 0 20.39-7.25 23.55-17.25-2.35.74-4.85 1.14-7.44 1.14z"
           class="ldl-ani"
-          style="stroke-width:1.19px;fill:#f3f8cf;stroke:#323334"
+          style="stroke-width:1.19px;fill:#ddde9b;stroke:#2c445c"
         /></g
       ><g
         class="ldl-layer"
-        style="transform-box:view-box;opacity:1;transform-origin:50px 50px 0;transform:matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,10,0,0,1);animation:14.2857s linear -14.2857s infinite forwards animate"
+        style="transform-box:view-box;opacity:1;transform-origin:50px 50px 0;transform:matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,10,0,0,1);animation:20s linear -20s infinite forwards animate"
         ><path
           fill="#A0C8D7"
           stroke="#323334"
@@ -43,7 +43,7 @@
           stroke-width="3.5"
           d="M17.45 61.49c-.1-.78-.16-1.58-.16-2.39 0-10.1 8.19-18.3 18.3-18.3 7.73 0 14.33 4.79 17.01 11.56 1.53-.77 3.25-1.21 5.08-1.21 5.84 0 10.64 4.39 11.31 10.05 5.4 1.35 9.41 6.22 9.41 12.04 0 6.75-5.38 12.23-12.08 12.41v.02H21.43C14.56 85.68 9 80.11 9 73.25c0-5.47 3.54-10.1 8.45-11.76z"
           class="ldl-ani"
-          style="stroke-width:1.19px;fill:#71bad6;stroke:#323334"
+          style="stroke-width:1.19px;fill:#408098;stroke:#2c445c"
         /></g
       ></g
     ></g
