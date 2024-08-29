d6478a95297df0a46e975297e884085764140f87
Dan Grebb <git@dgrebb.com>
2024-08-28 22:04:03 -0400

    style(weather): update `NightClear`

--- src/lib/components/WeatherIcons/NightClear.svelte
+++ src/lib/components/WeatherIcons/NightClear.svelte
@@ -1,61 +1,97 @@
-<script lang="ts">
-</script>
-
 <svg
+  xml:space="preserve"
   viewBox="0 0 100 100"
+  y="0px"
+  x="0px"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
   xmlns="http://www.w3.org/2000/svg"
+  id="Layer_1"
+  version="1.1"
+  style="margin: initial; display: block; shape-rendering: auto; background: transparent;"
+  preserveAspectRatio="xMidYMid"
   width="200"
   height="200"
-  style="display: block; background: transparent;"
->
-  <g style="transform-origin: 50% 50%; transform: rotate(0deg) scale(0.8);">
-    <g>
-      <g
-        style="transform-box: view-box; transform-origin: 50px 50px; transform: matrix3d(0.995, -0.105, 0, 0, 0.105, 0.995, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); animation: 6.25s linear -4.6875s infinite forwards animate;"
-      >
-        <path
-          d="M56.48,70.7c-15.01,0-27.18-12.17-27.18-27.18c0-2.98,0.48-5.84,1.37-8.52C19.83,38.57,12,48.78,12,60.82C12,75.83,24.17,88,39.18,88C51.22,88,61.43,80.17,65,69.33C62.32,70.22,59.46,70.7,56.48,70.7z"
-          fill="#FFF7A9"
-          stroke="#DCD155"
-          stroke-width="0.63"
-        />
-      </g>
-    </g>
-    <g
-      style="transform-box: view-box; transform-origin: 50px 50px; transform: matrix3d(0.995, -0.105, 0, 0, 0.105, 0.995, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); animation: 6.25s linear -6.25s infinite forwards animate;"
-    >
-      <polygon
-        points="52,11.19 56.16,19.62 65.47,20.98 58.73,27.54 60.32,36.81 52,32.43 43.68,36.81 45.27,27.54 38.53,20.98 47.84,19.62"
-        fill="#E8FCFF"
-        stroke="#DCD155"
-        stroke-width="0.63"
-      />
-      <polygon
-        points="72.71,43.72 78.56,46.79 84.41,43.72 83.29,50.23 88.02,54.84 81.48,55.79 78.56,61.72 75.63,55.79 69.1,54.84 73.83,50.23"
-        fill="#E8FCFF"
-        stroke="#DCD155"
-        stroke-width="0.63"
-      />
-    </g>
-  </g>
-  <style>
-    @keyframes animate {
-      0% {
-        transform: rotate(-6deg);
+  ><g
+    style="transform-origin: 50% 50% 0px; transform: rotate(0deg) scale(0.8);"
+    class="ldl-scale"
+    ><g class="ldl-ani"
+      ><g class="ldl-layer"
+        ><g
+          class="ldl-ani"
+          style="transform-box: view-box; opacity: 1; transform-origin: 50px 50px 0px; transform: matrix3d(0.998, -0.07, 0, 0, 0.07, 0.998, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); animation: 1s linear -0.666667s infinite forwards nightClearAnimate;"
+          ><path
+            d="M56.48,70.7c-15.01,0-27.18-12.17-27.18-27.18c0-2.98,0.48-5.84,1.37-8.52C19.83,38.57,12,48.78,12,60.82 C12,75.83,24.17,88,39.18,88C51.22,88,61.43,80.17,65,69.33C62.32,70.22,59.46,70.7,56.48,70.7z"
+            stroke-miterlimit="10"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="3.5"
+            stroke="#323334"
+            fill="#FEE1C5"
+            style="stroke-width: 1.33px; fill: rgb(254, 225, 197); stroke: rgb(23, 75, 126);"
+          ></path></g
+        ></g
+      ><g class="ldl-layer"
+        ><g class="ldl-ani"
+          ><g
+            ><g class="ldl-layer"
+              ><g
+                class="ldl-ani"
+                style="transform-box: view-box; opacity: 1; transform-origin: 50px 50px 0px; transform: matrix3d(0.998, -0.07, 0, 0, 0.07, 0.998, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); animation: 1s linear -0.833333s infinite forwards nightClearAnimate;"
+                ><polygon
+                  points="52,11.19 56.16,19.62 65.47,20.98 58.73,27.54 60.32,36.81 52,32.43 43.68,36.81 45.27,27.54 38.53,20.98 47.84,19.62"
+                  stroke-miterlimit="10"
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke-width="3.5"
+                  stroke="#323334"
+                  fill="#F5E169"
+                  style="stroke-width: 1.33px; fill: rgb(245, 225, 105); stroke: rgb(23, 75, 126);"
+                ></polygon></g
+              ></g
+            ><g class="ldl-layer"
+              ><g
+                class="ldl-ani"
+                style="transform-box: view-box; opacity: 1; transform-origin: 50px 50px 0px; transform: matrix3d(0.998, -0.07, 0, 0, 0.07, 0.998, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); animation: 1s linear -1s infinite forwards nightClearAnimate;"
+                ><polygon
+                  points="72.71,43.72 78.56,46.79 84.41,43.72 83.29,50.23 88.02,54.84 81.48,55.79 78.56,61.72 75.63,55.79 69.1,54.84 73.83,50.23"
+                  stroke-miterlimit="10"
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke-width="3.5"
+                  stroke="#323334"
+                  fill="#F5E169"
+                  style="stroke-width: 1.33px; fill: rgb(245, 225, 105); stroke: rgb(23, 75, 126);"
+                ></polygon></g
+              ></g
+            ></g
+          ></g
+        ></g
+      ></g
+    ></g
+  ><style type="text/css">
+    @keyframes nightClearAnimate {
+      0.00% {
+        transform: translate(0px, 0px) rotate(-4deg) scale(1, 1)
+          skew(0deg, 0deg);
         opacity: 1;
+        animation-timing-function: cubic-bezier(0.59, 0.02, 0.79, 0.52);
       }
-      26% {
-        transform: rotate(0.6deg);
+      26.00% {
+        transform: translate(0px, 0px) rotate(0.4deg);
+        animation-timing-function: cubic-bezier(0.19, 0.49, 0.42, 0.98);
       }
-      50% {
-        transform: rotate(6deg);
+      50.00% {
+        transform: translate(0px, 0px) rotate(4deg);
+        animation-timing-function: cubic-bezier(0.59, 0.02, 0.79, 0.52);
       }
-      76% {
-        transform: rotate(-0.6deg);
+      76.00% {
+        transform: translate(0px, 0px) rotate(-0.4deg);
+        animation-timing-function: cubic-bezier(0.21, 0.48, 0.44, 0.93);
       }
-      100% {
-        transform: rotate(-6deg);
+      100.00% {
+        transform: translate(0px, 0px) rotate(-4deg);
+        animation-timing-function: cubic-bezier(0.21, 0.48, 0.44, 0.93);
       }
     }
-  </style>
-</svg>
+  </style></svg
+>
